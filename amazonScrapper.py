479d52e065902705246caa46e9d8ae790cd947e4
Yash dhadve <yashdhadwe@gmail.com>
2023-01-19 08:21:27 +0530

    errors were fixed of scrapping amazon website

--- amazonScrapper.py
+++ amazonScrapper.py
@@ -6,20 +6,29 @@ import requests
 headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.1 Safari/605.1.15'}
 
 AmazonSearchBarUrl = 'https://www.amazon.in/s?k='
+MintraSearchBarUrl = 'https://www.myntra.com/'
+FlipkartSearchBarUrl = 'https://www.flipkart.com/search?q='
 
 SearchThis = input("What do want to search : ")
 
-mainPage = requests.get(AmazonSearchBarUrl+SearchThis, headers=headers).text
-
-soup = BeautifulSoup(mainPage, 'lxml')
-
-output = soup.find('div',class_='sg-col-4-of-24 sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 AdHolder sg-col s-widget-spacing-small sg-col-4-of-20')
-
-output_brand = output.find('span',class_='a-size-base-plus a-color-base').text
-output_price = output.find('span', class_='a-price-whole').text
-output_productName = output.find('span', class_='a-size-base-plus a-color-base a-text-normal').text
+PageAmazon = requests.get(AmazonSearchBarUrl+SearchThis, headers=headers).text
+PageMintra = requests.get(MintraSearchBarUrl+SearchThis, headers= headers).text
+pageFlipkart = requests.get(FlipkartSearchBarUrl+SearchThis, headers=headers).text
 
 
-print('Product : ',output_productName)
-print('Brand Name : ',output_brand)
-print('Price : ',output_price)
+soupAmazon = BeautifulSoup(PageAmazon, 'lxml')
+soupMintra = BeautifulSoup(PageMintra, 'lxml')
+soupFlipkart = BeautifulSoup(pageFlipkart, 'lxml')
+
+# Getting Link of particular product from main search page
+outputAmazonMainDiv = soupAmazon.find('div',class_='s-widget-container s-spacing-small s-widget-container-height-small celwidget slot=MAIN template=SEARCH_RESULTS widgetId=search-results_1')
+outputAmazonProductInfo = outputAmazonMainDiv.find('div', class_="a-section")
+AmazonProduct = outputAmazonProductInfo.find('a', class_='a-link-normal s-no-outline')
+
+AmazonProductName = outputAmazonMainDiv.find('h2').text
+AmazonProductPrice = outputAmazonMainDiv.find('span', class_= 'a-price').find('span').text
+AmazonProductLink ='https://www.amazon.in/' + AmazonProduct['href']
+
+print(AmazonProductName)
+print(AmazonProductPrice)
+print(AmazonProductLink)
